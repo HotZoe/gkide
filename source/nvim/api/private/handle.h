c89e1233b9acd9bab174cb37fa232525bd29634f
Charlie WONG <charlie-wong@foxmail.com>
2017-09-28 00:14:02 +0800

    style: coding style changing, make the big macros more readable
    Signed-off-by: Charlie WONG <charlie-wong@foxmail.com>

--- source/nvim/api/private/handle.h
+++ source/nvim/api/private/handle.h
@@ -7,16 +7,24 @@
 #include "nvim/buffer_defs.h"
 #include "nvim/api/private/defs.h"
 
-#define HANDLE_DECLS(type, name) \
+#define HANDLE_DECLS(type, name)              \
     type *handle_get_##name(handle_T handle); \
-    void handle_register_##name(type *name); \
+    void handle_register_##name(type *name);  \
     void handle_unregister_##name(type *name);
 
-// handle_get_buffer handle_register_buffer, handle_unregister_buffer
+// handle_get_buffer
+// handle_register_buffer
+// handle_unregister_buffer
 HANDLE_DECLS(buf_T, buffer)
-// handle_get_window handle_register_window, handle_unregister_window
+
+// handle_get_window
+// handle_register_window
+// handle_unregister_window
 HANDLE_DECLS(win_T, window)
-// handle_get_tabpage handle_register_tabpage, handle_unregister_tabpage
+
+// handle_get_tabpage
+// handle_register_tabpage
+// handle_unregister_tabpage
 HANDLE_DECLS(tabpage_T, tabpage)
 
 void handle_init(void);
