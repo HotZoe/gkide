36c1c86ebab4039b297f3300f76a24f8350cb29a
Charlie WONG <charlie-wong@foxmail.com>
2017-10-23 21:34:55 +0800

    style: lua script readability
    Signed-off-by: Charlie WONG <charlie-wong@foxmail.com>

--- source/nvim/generators/c_grammar.lua
+++ source/nvim/generators/c_grammar.lua
@@ -4,51 +4,63 @@ package.cpath = luapathcfg.cpath .. package.cpath
 
 lpeg = require('lpeg')
 
--- lpeg grammar for building api metadata from a set of header files. It
--- ignores comments and preprocessor commands and parses a very small subset
+-- lpeg grammar for building api metadata from a set of header files.
+-- It ignores comments and preprocessor commands and parses a very small subset
 -- of C prototypes with a limited set of types
-local P, R, S = lpeg.P, lpeg.R, lpeg.S
-local C, Ct, Cc, Cg = lpeg.C, lpeg.Ct, lpeg.Cc, lpeg.Cg
 
-local any = P(1) -- (consume one character)
+local P  = lpeg.P  -- lpeg.P(string)  Matches string literally
+local R  = lpeg.R  -- lpeg.R("xy")    Matches any character between x and y (Range)
+local S  = lpeg.S  -- lpeg.S(string)  Matches any character in string (Set)
+
+local C  = lpeg.C  -- lpeg.C(patt)            the match for patt plus all captures made by patt
+local Ct = lpeg.Ct -- lpeg.Ct(patt)           a table with all captures from patt
+local Cc = lpeg.Cc -- lpeg.Cc(values)         the given values (matches the empty string)
+local Cg = lpeg.Cg -- lpeg.Cg(patt [, name])  the values produced by patt, optionally tagged with name
+
+local any    = P(1) -- consume one character
+
+local num    = R('09') -- match any number character
 local letter = R('az', 'AZ') + S('_$')
-local num = R('09')
-local alpha = letter + num
-local nl = P('\r\n') + P('\n')
+local alpha  = letter + num
+
+local nl     = P('\r\n') + P('\n')
 local not_nl = any - nl
-local ws = S(' \t') + nl
+
+local ws   = S(' \t') + nl
 local fill = ws ^ 0
+
 local c_comment = P('//') * (not_nl ^ 0)
-local c_preproc = P('#') * (not_nl ^ 0)
-local typed_container =
-  (P('ArrayOf(') + P('DictionaryOf(')) * ((any - P(')')) ^ 1) * P(')')
-local c_id = (
-  typed_container +
-  (letter * (alpha ^ 0))
-)
+local c_preproc = P('#')  * (not_nl ^ 0)
+
+local typed_container = (P('ArrayOf(') + P('DictionaryOf(')) * ((any - P(')')) ^ 1) * P(')')
+
+local c_id   = (typed_container + (letter * (alpha ^ 0)))
 local c_void = P('void')
-local c_param_type = (
-  ((P('Error') * fill * P('*') * fill) * Cc('error')) +
-  (C(c_id) * (ws ^ 1))
-  )
+
+local c_param_type = (((P('Error') * fill * P('*') * fill) * Cc('error')) + (C(c_id) * (ws ^ 1)))
 local c_type = (C(c_void) * (ws ^ 1)) + c_param_type
+
 local c_param = Ct(c_param_type * C(c_id))
 local c_param_list = c_param * (fill * (P(',') * fill * c_param) ^ 0)
 local c_params = Ct(c_void + c_param_list)
+
 local c_proto = Ct(
-  Cg(c_type, 'return_type') * Cg(c_id, 'name') *
-  fill * P('(') * fill * Cg(c_params, 'parameters') * fill * P(')') *
-  Cg(Cc(false), 'async') *
-  (fill * Cg((P('FUNC_API_SINCE(') * C(num ^ 1)) * P(')'), 'since') ^ -1) *
-  (fill * Cg((P('FUNC_API_DEPRECATED_SINCE(') * C(num ^ 1)) * P(')'),
-              'deprecated_since') ^ -1) *
-  (fill * Cg((P('FUNC_API_ASYNC') * Cc(true)), 'async') ^ -1) *
-  (fill * Cg((P('FUNC_API_NOEXPORT') * Cc(true)), 'noexport') ^ -1) *
-  (fill * Cg((P('FUNC_API_REMOTE_ONLY') * Cc(true)), 'remote_only') ^ -1) *
-  (fill * Cg((P('FUNC_API_REMOTE_IMPL') * Cc(true)), 'remote_impl') ^ -1) *
-  (fill * Cg((P('FUNC_API_BRIDGE_IMPL') * Cc(true)), 'bridge_impl') ^ -1) *
-  fill * P(';')
-  )
+    Cg(c_type, 'return_type') *  -- function return type
+    Cg(c_id, 'name') *           -- function name
+    fill * P('(') * fill *       -- the open parentheses
+    Cg(c_params, 'parameters') * -- the function arguments list
+    fill * P(')') *              -- the close parentheses
+    Cg(Cc(false), 'async') *
+    (fill * Cg((P('FUNC_API_SINCE(')            * C(num ^ 1)) * P(')'), 'since')            ^ -1) *
+    (fill * Cg((P('FUNC_API_DEPRECATED_SINCE(') * C(num ^ 1)) * P(')'), 'deprecated_since') ^ -1) *
+    (fill * Cg((P('FUNC_API_ASYNC')       * Cc(true)), 'async')       ^ -1) *
+    (fill * Cg((P('FUNC_API_NOEXPORT')    * Cc(true)), 'noexport')    ^ -1) *
+    (fill * Cg((P('FUNC_API_REMOTE_ONLY') * Cc(true)), 'remote_only') ^ -1) *
+    (fill * Cg((P('FUNC_API_REMOTE_IMPL') * Cc(true)), 'remote_impl') ^ -1) *
+    (fill * Cg((P('FUNC_API_BRIDGE_IMPL') * Cc(true)), 'bridge_impl') ^ -1) *
+    fill * P(';')
+    )
 
 local grammar = Ct((c_proto + c_comment + c_preproc + ws) ^ 1)
-return {grammar=grammar, typed_container=typed_container}
+
+return { grammar=grammar, typed_container=typed_container }
