fe5010179d9c64723bdb1c3b3756bbb339991903
Charlie WONG <charlie-wong@foxmail.com>
2017-11-11 17:34:40 +0800

    chore: ex lua generated style only
    Signed-off-by: Charlie WONG <charlie-wong@foxmail.com>

--- source/nvim/generators/gen_ex_cmds.lua
+++ source/nvim/generators/gen_ex_cmds.lua
@@ -3,15 +3,15 @@ local includedir = arg[2]
 local autodir = arg[3]
 
 if nvimsrcdir == '--help' then
-  print ([[
+    print ([[
 Usage:
-  lua genex_cmds.lua src/nvim build/include build/src/nvim/auto
+  lua gen_ex_cmds.lua source/nvim build/include build/src/nvim/auto
 
 Will generate files build/include/ex_cmds_enum.generated.h with cmdidx_T 
 enum and build/src/nvim/auto/ex_cmds_defs.generated.h with main Ex commands 
 definitions.
 ]])
-  os.exit(0)
+    os.exit(0)
 end
 
 package.path = nvimsrcdir .. '/?.lua;' .. package.path
@@ -40,51 +40,61 @@ static const cmdidx_T cmdidxs[%u] = {
 enumfile:write([[
 typedef enum CMD_index {
 ]])
+
 defsfile:write(string.format([[
 static CommandDefinition cmdnames[%u] = {
 ]], #defs))
+
 for i, cmd in ipairs(defs) do
-  local enumname = cmd.enum or ('CMD_' .. cmd.command)
-  firstchar = string.byte(cmd.command)
-  if firstchar ~= prevfirstchar then
-    if (not prevfirstchar
-        or (byte_a <= firstchar      and firstchar     <= byte_z)
-        or (byte_a <= prevfirstchar  and prevfirstchar <= byte_z)) then
-      if not first then
-        cmdidxs = cmdidxs .. ',\n'
-      end
-      cmdidxs = cmdidxs .. '  ' .. enumname
+    local enumname = cmd.enum or ('CMD_' .. cmd.command)
+    firstchar = string.byte(cmd.command)
+    
+    if firstchar ~= prevfirstchar then
+        if(not prevfirstchar
+           or (byte_a <= firstchar      and firstchar     <= byte_z)
+           or (byte_a <= prevfirstchar  and prevfirstchar <= byte_z)) then
+            if not first then
+                cmdidxs = cmdidxs .. ',\n'
+            end
+            cmdidxs = cmdidxs .. '    ' .. enumname
+        end
+        prevfirstchar = firstchar
     end
-    prevfirstchar = firstchar
-  end
-  if first then
-    first = false
-  else
-    defsfile:write(',\n')
-  end
-  enumfile:write('  ' .. enumname .. ',\n')
-  defsfile:write(string.format([[
-  [%s] = {
+    
+    if first then
+        first = false
+    else
+        defsfile:write(',\n')
+    end
+    
+    enumfile:write('    ' .. enumname .. ',\n')
+    defsfile:write(string.format([[
+[%s] = {
     .cmd_name = (char_u *) "%s",
     .cmd_func = (ex_func_T)&%s,
     .cmd_argt = %uL,
     .cmd_addr_type = %i
-  }]], enumname, cmd.command, cmd.func, cmd.flags, cmd.addr_type))
+    }]], enumname, cmd.command, cmd.func, cmd.flags, cmd.addr_type))
 end
+
 defsfile:write([[
 
 };
+
 ]])
+
 enumfile:write([[
-  CMD_SIZE,
-  CMD_USER = -1,
-  CMD_USER_BUF = -2
+    CMD_SIZE,
+    CMD_USER = -1,
+    CMD_USER_BUF = -2
 } cmdidx_T;
 ]])
+
 cmdidxs = cmdidxs .. [[
 
 };
 ]]
+
 defsfile:write(cmdidxs)
 
 enumfile:close()
