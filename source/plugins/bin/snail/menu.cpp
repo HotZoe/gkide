881870712adc4a4304c77fca24d3ed95072b8804
Charlie WONG <charlie-wong@foxmail.com>
2018-03-04 04:34:53 +0800

    fix: merge menu fix for windows build
    Signed-off-by: Charlie WONG <charlie-wong@foxmail.com>

--- source/plugins/bin/snail/menu.cpp
+++ source/plugins/bin/snail/menu.cpp
@@ -10,35 +10,39 @@ GkideMenu* GkideMenu::m_instance = NULL;
 GkideMenu::GkideMenu(MainWindow *mw)
     : m_mainwin(mw)
 {
-    // menu bar
+    // menubar
     m_menuBar = new QMenuBar(mw);
-
-    QAction *ptr = NULL;
+    mw->setMenuBar(m_menuBar);
 
     // File
-    m_fileMenu = new QMenu(QObject::tr("File"));
-    m_fileMenu->setParent(m_menuBar);
-    m_menuBar->addMenu(m_fileMenu);
-    // file->New File or Project ...
-    ptr = new QAction("New File or Project ...", m_fileMenu);
-    ptr->setShortcut(Qt::CTRL | Qt::Key_N);
-    m_fileMenu->addAction(ptr);
-    // file->Open File or Project ...
-    ptr = new QAction("Open File or Project ...", m_fileMenu);
-    ptr->setShortcut(Qt::CTRL | Qt::Key_O);
-    m_fileMenu->addAction(ptr);
-    //m_fileMenu->addSeparator();
-    //m_fileMenu->addSection("Section");
+    m_fileMenu = new QMenu(m_menuBar);
+    m_fileMenu->setTitle(QObject::tr("File"));
 
     // Help
     m_helpMenu = new QMenu(QObject::tr("Help"));
-    m_helpMenu->setParent(m_menuBar);
-    m_menuBar->addMenu(m_helpMenu);
+    m_helpMenu->setTitle(QObject::tr("Help"));
+
+    // File->New File or Project ...
+    QAction *file_NewFileProject = new QAction(mw);
+    file_NewFileProject->setText("New File or Project ...");
+    file_NewFileProject->setShortcut(Qt::CTRL | Qt::Key_N);
+
+    // File->Open File or Project ...
+    QAction *file_OpenFileProject = new QAction(mw);
+    file_OpenFileProject->setText("Open File or Project ...");
+    file_OpenFileProject->setShortcut(Qt::CTRL | Qt::Key_O);
+
     // Help->About GKIDE
-    ptr = new QAction("About GKIDE", m_helpMenu);
-    m_helpMenu->addAction(ptr);
-    QObject::connect(ptr, &QAction::triggered,
+    QAction *help_AboutGKIDE = new QAction(mw);
+    help_AboutGKIDE->setText("About GKIDE");
+    QObject::connect(help_AboutGKIDE, &QAction::triggered,
                      this, &GkideMenu::trigerHelpAboutGKIDE);
+
+    m_menuBar->addAction(m_fileMenu->menuAction());
+    m_fileMenu->addAction(file_NewFileProject);
+    m_fileMenu->addAction(file_OpenFileProject);
+    m_menuBar->addAction(m_helpMenu->menuAction());
+    m_helpMenu->addAction(help_AboutGKIDE);
 }
 
 GkideMenu *GkideMenu::getGkideMenuInstance(MainWindow *mw, bool redo)
